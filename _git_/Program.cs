77798a8edee25878cc4cc62e88c317917b6f2ed8
Gustavo Guerreiro <gustavo.carvalhal.guerreiro@gmail.com>
2019-10-14 17:11:36 -0300

    2commit

--- _git_/Program.cs
+++ _git_/Program.cs
@@ -4,6 +4,7 @@ namespace _git_ {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("TESTE GIT");
+            Console.WriteLine("TESTE GIT 2");
         }
     }
 }
